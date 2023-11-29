44d92e633470baef3b91b4e4d4bcc25a5830efce
SainAA <gnasainaa4@gmail.com>
2023-11-29 15:01:42 +0800

    started todo

--- ex box/ox.js
+++ ex box/ox.js
@@ -30,13 +30,26 @@ const myArray = [
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAaRuRrEv3vECsWQ_Gq3kLFayXKw0fi_NYnA&usqp=CAU",
   },
-  { name: "Vegeta" },
-  { DOB: "prince of sayajin" },
-  { IDno: "12334536" },
-  { issued: "January 2011" },
-  { expires: " Dcember 2013" },
+  {
+    mainInfo: [
+      { name: "Vegeta" },
+      { DOB: "Sayajin" },
+      { IDno: "12334536" },
+      { issued: "January2011" },
+      { expires: " Dcember2013" },
+    ],
+  },
+
   { Bname: "Vegeta" },
   { POS: "Maintenanse Services" },
+
+  // { name: "Vegeta" },
+  // { DOB: "Sayajin" },
+  // { IDno: "12334536" },
+  // { issued: "January2011" },
+  // { expires: " Dcember2013" },
+  // { Bname: "Vegeta" },
+  // { POS: "Maintenanse Services" },
 ];
 const Container = document.getElementsByClassName("container")[0];
 Container.style.display = "flex";
@@ -108,6 +121,181 @@ upperImg.style.border = "7px solid blue";
 /// right of dood
 const rightDood = document.createElement("div");
 dood.appendChild(rightDood);
-rightDood.style.width = "330px";
+rightDood.style.width = "360px";
 rightDood.style.height = "260px";
-rightDood.style.border = "1px solid red";
+rightDood.style.marginTop = "13px";
+
+rightDood.style.display = "flex";
+rightDood.style.alignItems = "center";
+rightDood.style.flexDirection = "column";
+////
+
+//const rigthSide = document.createElement("div");
+//const lowerDiv = document.createElement("div");
+const innerRight = document.createElement("div");
+innerRight.style.height = "250px";
+innerRight.style.width = "280px";
+innerRight.style.gap = "3px";
+innerRight.style.display = "flex";
+innerRight.style.flexDirection = "column";
+
+rightDood.appendChild(innerRight);
+// lowerDiv.append( rigthSide);
+// cardDiv.appendChild(lowerDiv);
+// lowerDiv.style.display = "flex";
+// const image = document.createElement("img");
+// myInfoArray.forEach((el) => {
+//   if (el.image && el.image != undefined) {
+//     image.src = el.image;
+//   }
+// });
+// leftSide.appendChild(image);
+const mainInfo = myArray.filter((el) => {
+  if (el.mainInfo && el.mainInfo != undefined) {
+    return true;
+  }
+});
+const realMainInfo = mainInfo[0].mainInfo;
+const realMainKeys = realMainInfo.map((el) => {
+  const result = Object.keys(el)[0];
+  return result;
+});
+realMainKeys.forEach((el, index) => {
+  const infoDiv = document.createElement("div");
+  const infoDivLeftChild = document.createElement("div");
+  const infoDivRigthChild = document.createElement("div");
+  infoDiv.style.display = "flex";
+  infoDiv.style.justifyContent = "space-between";
+  infoDivLeftChild.innerHTML = `${el}:`;
+  infoDivRigthChild.innerHTML = realMainInfo[index][el];
+  infoDiv.append(infoDivLeftChild, infoDivRigthChild);
+  innerRight.appendChild(infoDiv);
+  infoDivLeftChild.style.fontSize = "18px";
+  infoDivLeftChild.style.fontFamily = "roboto";
+  infoDivLeftChild.style.color = "grey";
+
+  //
+  infoDivRigthChild.style.display = "flex";
+  infoDivRigthChild.style.justifyContent = "flex-start";
+  infoDivRigthChild.style.width = "170px";
+  infoDivRigthChild.style.fontSize = "24px";
+  infoDivRigthChild.style.fontFamily = "roboto";
+});
+
+///one
+// const oneOfBottom = document.createElement("div");
+// rightDood.appendChild(oneOfBottom);
+
+// myArray.forEach((el) => {
+//   if (el.name != "" && el.name != undefined) {
+//     oneOfBottom.innerHTML = ` name:            ${el.name}`;
+//   }
+// });
+
+// oneOfBottom.style.width = "280px";
+// oneOfBottom.style.height = "30px";
+// oneOfBottom.style.wordSpacing = "33px";
+// oneOfBottom.style.fontSize = "20px";
+// oneOfBottom.style.fontFamily = "roboto";
+// oneOfBottom.style.display = "flex";
+// oneOfBottom.style.justifyContent = "spaceAround";
+// //two
+// const twoOfBottom = document.createElement("div");
+// rightDood.appendChild(twoOfBottom);
+
+// myArray.forEach((el) => {
+//   if (el.DOB != "" && el.DOB != undefined) {
+//     twoOfBottom.innerHTML = ` D.O.B:            ${el.DOB}`;
+//   }
+// });
+
+// twoOfBottom.style.width = "280px";
+// twoOfBottom.style.height = "30px";
+// twoOfBottom.style.wordSpacing = "34px";
+// twoOfBottom.style.fontSize = "20px";
+// twoOfBottom.style.fontFamily = "roboto";
+// twoOfBottom.style.display = "flex";
+// twoOfBottom.style.justifyContent = "spaceAround";
+// //three
+// const threeOfBottom = document.createElement("div");
+// rightDood.appendChild(threeOfBottom);
+
+// myArray.forEach((el) => {
+//   if (el.IDno != "" && el.IDno != undefined) {
+//     threeOfBottom.innerHTML = ` IDNo:            ${el.IDno}`;
+//   }
+// });
+
+// threeOfBottom.style.width = "280px";
+// threeOfBottom.style.height = "30px";
+// threeOfBottom.style.wordSpacing = "35px";
+// threeOfBottom.style.fontSize = "20px";
+// threeOfBottom.style.fontFamily = "roboto";
+// threeOfBottom.style.display = "flex";
+// threeOfBottom.style.justifyContent = "spaceAround";
+// //four
+
+// const fourOfBottom = document.createElement("div");
+// rightDood.appendChild(fourOfBottom);
+
+// myArray.forEach((el) => {
+//   if (el.issued != "" && el.issued != undefined) {
+//     fourOfBottom.innerHTML = ` Issued:            ${el.issued}`;
+//   }
+// });
+
+// fourOfBottom.style.width = "280px";
+// fourOfBottom.style.height = "30px";
+// fourOfBottom.style.wordSpacing = "20px";
+// fourOfBottom.style.fontSize = "20px";
+// fourOfBottom.style.fontFamily = "roboto";
+// fourOfBottom.style.display = "flex";
+// fourOfBottom.style.justifyContent = "spaceAround";
+// // five;
+// const fiveOfBottom = document.createElement("div");
+// rightDood.appendChild(fiveOfBottom);
+
+// myArray.forEach((el) => {
+//   if (el.expires != "" && el.expires != undefined) {
+//     fiveOfBottom.innerHTML = ` expires:            ${el.expires}`;
+//   }
+// });
+
+// fiveOfBottom.style.width = "280px";
+// fiveOfBottom.style.height = "30px";
+// fiveOfBottom.style.wordSpacing = "16px";
+// fiveOfBottom.style.fontSize = "20px";
+// fiveOfBottom.style.fontFamily = "roboto";
+// fiveOfBottom.style.display = "flex";
+// fiveOfBottom.style.justifyContent = "spaceAround";
+// //six
+const sixOfBottom = document.createElement("div");
+innerRight.appendChild(sixOfBottom);
+myArray.forEach((el) => {
+  if (el.Bname != "" && el.Bname != undefined) {
+    sixOfBottom.innerHTML = el.Bname;
+  }
+});
+sixOfBottom.style.fontSize = "40px";
+
+sixOfBottom.style.width = "240px";
+// sixOfBottom.style.height = "50px";
+
+sixOfBottom.style.color = "blue";
+sixOfBottom.style.marginLeft = "40px";
+
+sixOfBottom.style.fontFamily = "roboto";
+//seven
+const sevenOfBottom = document.createElement("div");
+innerRight.appendChild(sevenOfBottom);
+myArray.forEach((el) => {
+  if (el.POS != "" && el.POS != undefined) {
+    sevenOfBottom.innerHTML = el.POS;
+  }
+});
+sevenOfBottom.style.fontSize = "20px";
+
+sevenOfBottom.style.width = "280px";
+// sevenOfBottom.style.height = "30px";
+
+sevenOfBottom.style.fontFamily = "roboto";
