c32dee291e369ed93fbb6763641c54067e4e9d4f
SainAA <gnasainaa4@gmail.com>
2023-12-01 13:23:12 +0800

    added huired

--- toDo/toDo.js
+++ toDo/toDo.js
@@ -7,8 +7,9 @@ const description = document.getElementById("bottonOFdescription");
 
 const status2 = document.querySelectorAll("#bottonOFStatus")[0];
 const Priority = document.getElementById("bottonOFPriority");
-console.log(status2);
+// console.log(status2);
 const addTask = document.getElementById("addTsk");
+const parentDiv = document.getElementById("parentdiv");
 
 // display: block
 addCard.forEach((el) => {
@@ -23,23 +24,50 @@ window.onclick = function (event) {
   }
 };
 //
-let ttle = "";
-title.addEventListener("change", () => {
-  ttle = title.value;
-  console.log(ttle);
-});
 
-let desc = "";
-description.addEventListener("change", () => {
-  desc = description.value;
-  console.log(desc);
-});
-
-addTask.addEventListener("click", () => {
-  var temp = status2.value;
-
-  console.log(temp);
-  var pri = Priority.value;
-  console.log(pri);
+const states = [];
+let myObject = () => {
+  (title.value = ""),
+    (description.value = ""),
+    (status2.value = "To-do"),
+    (Priority.value = "low");
+};
+addTask.addEventListener("click", (newTask) => {
+  newTask = {
+    title: title.value,
+    description: description.value,
+    status: status2.value,
+    Priority: Priority.value,
+  };
+  states.push(newTask); 
+  console.log(states);
   big.style.display = "none";
+
+  myObject();
 });
+// title.addEventListener("change", (event) => {
+//   myObject.title = event.target.value;
+// });
+
+// description.addEventListener("change", (event) => {
+//   myObject.description = event.target.value;
+// });
+
+// addTask.addEventListener("click", () => {
+//   var temp = status2.value;
+//   myObject.status = temp;
+
+//   var pri = Priority.value;
+
+//   myObject.Priority = pri;
+//   big.style.display = "none";
+//   states.push(myObject);
+//   console.log(states);
+//   myObject.title = "";
+//   myObject.description = "";
+//   status2.value = "";
+//   Priority.value = "";
+// });
+////function///////////////////////////////////////////
+
+//
