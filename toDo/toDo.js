13c3ee57b6c596dc332fd249f6195d0731790692
SainAA <gnasainaa4@gmail.com>
2023-12-05 14:53:57 +0800

    gatsuur

--- toDo/toDo.js
+++ toDo/toDo.js
@@ -5,11 +5,12 @@ const description = document.getElementById("bottonOFdescription");
 
 // const Status = document.getElementById("bottonOFStatus");
 
-const status2 = document.querySelectorAll("#bottonOFStatus")[0];
+const status2 = document.getElementById("bottonOFStatus");
 const Priority = document.getElementById("bottonOFPriority");
 // console.log(status2);
 const addTask = document.getElementById("addTsk");
 const parentDiv = document.getElementById("parentdiv");
+const containerr = document.getElementById("middle");
 
 // display: block
 addCard.forEach((el) => {
@@ -40,9 +41,49 @@ addTask.addEventListener("click", (newTask) => {
     Priority: Priority.value,
   };
   states.push(newTask);
-  console.log(states);
+
+  let temp = JSON.stringify(states);
+  localStorage.setItem("keyToDo", temp);
+
   big.style.display = "none";
 
   myObject();
 });
+const card = (prop) => {
+  const { title, description, Priority } = prop;
 
+  return `<div id="ass">
+               <div id="currect">
+                  <div id="in" >&#10003</div>
+               </div>
+               <div id="rightOfMiddle">
+                  <div id="midOfMid"> 
+                   <h2>${title}</h2>
+                   <h2>${description}</h2>
+                   <div id="leftOfMid"> ${Priority}</div>
+   
+                  </div>
+                  <div class="atLast">
+                     <div id="ex">&#215;</div>
+                     <div id="note">&#x2660;</div>
+                   </div>
+               </div>
+            
+    
+            </div>
+            
+    `;
+};
+const render = () => {
+  // local get
+  const response = JSON.parse(localStorage.getItem("keyToDo"));
+  console.log(response);
+  // response -> for foreach map -> el -> cacrd(el) -> result
+  // middiv.innerhtl += result
+  response.forEach((el) => {
+    const result = card(el);
+    let ass = result;
+    containerr.innerHTML += ass;
+  });
+};
+render();
